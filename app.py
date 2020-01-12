6a43809951d3b07a48452ac1f3f6143d45cc117e
DimaKutas <dima9satuk@gmail.com>
2020-01-12 22:22:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def refresh_db(cur, conn):
 			print(row)
 			if row[0] != 'Name':
 				cur.execute("""insert or ignore into water(Name, Ca, Mg, F) 
-				values (?, ?, ?, ?)""", (' '.join(row[0], row[1], row[2], row[3])))
+				values (?, ?, ?, ?)""", (row[0], row[1], row[2], row[3]))
 				conn.commit()
 			
 	return jsonify({'Action': 'Refresh data', 'State': 'Success'})
