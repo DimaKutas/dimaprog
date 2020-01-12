4339dea13a2c0b38cfa46c304a5fa45149ac1707
DimaKutas <dima9satuk@gmail.com>
2020-01-12 21:31:07 +0200

    fix
    a lot

--- app.py
+++ app.py
@@ -14,185 +14,197 @@ app.secret_key = b'r2q35b4536y5yasdfg5656y98543h394yhtr7834t3490tsrtg'
 
 @auth.verify_password
 def verify_password(username, password):
-    conn = sqlite3.connect('db.db')
-    cur = conn.cursor()
-    cur.execute('select * from users where username = ?', (username, ))
-    try:
-        userdata = cur.fetchone()
-    except TypeError:  # If user not exists
-        session['userid'] = None
-        return False
-    is_authenticated = check_password_hash(userdata[2], password)
-    if is_authenticated:
-        session['userid'] = int(userdata[0])
-    else:
-        session['userid'] = None
-    conn.close()
-    return is_authenticated
+	conn = sqlite3.connect('db.db')
+	cur = conn.cursor()
+	cur.execute('select * from users where username = ?', (username))
+	try:
+		userdata = cur.fetchone()
+	except TypeError:  # If user not exists
+		session['userid'] = None
+		conn.close()
+		return False
+	is_authenticated = check_password_hash(userdata[2], password)
+	if is_authenticated:
+		session['userid'] = int(userdata[0])
+	else:
+		session['userid'] = None
+	conn.close()
+	return is_authenticated
 
 
 def db_connect(function_to_decorate):
-    def wrapper(*args, **kwargs):
-        conn = sqlite3.connect('db.db')
-        cur = conn.cursor()
-        res = function_to_decorate(cur, conn, *args, **kwargs)  # Сама функция
-        conn.close()
-        return res
-    wrapper.__name__ = function_to_decorate.__name__
-    return wrapper
+	def wrapper(*args, **kwargs):
+		conn = sqlite3.connect('db.db')
+		cur = conn.cursor()
+		res = function_to_decorate(cur, conn, *args, **kwargs)  # Сама функция
+		conn.close()
+		return res
+	wrapper.__name__ = function_to_decorate.__name__
+	return wrapper
 
 #delete all users even if you not logged in
 @app.route('/users_clear_force', methods=['GET'])
 @db_connect
 def force_refresh_users_db(cur, conn):
-    cur.execute('delete from users')
-    conn.commit()
-    return jsonify({'Action': 'Force users clear', 'State': 'Success'})
+	cur.execute('delete from users')
+	conn.commit()
+	return jsonify({'Action': 'Force users clear', 'State': 'Success'})
 
 #delete all users exepct current
 @app.route('/users_clear', methods=['GET'])
 @auth.login_required
 @db_connect
 def refresh_users_db(cur, conn):
-    cur.execute("delete from users where username not like '%" + auth.username() + "'")
-    conn.commit()
-    return jsonify({'Action': 'Users clear', 'State': 'Success'})
-    
-# delete from users where username not like '%auth.username()';
-
+	cur.execute("delete from users where username not like '%" + auth.username() + "'")
+	conn.commit()
+	return jsonify({'Action': 'Users clear', 'State': 'Success'})
+	
 @app.route('/db_refresh', methods=['POST'])
 @auth.login_required
 @db_connect
 def refresh_db(cur, conn):
-    cur.execute('delete from water')
-    conn.commit()
-    cur.execute("delete from sqlite_sequence where name='water'")
-    conn.commit()
-    with open('water.csv') as csvfile:
-        readCSV = csv.reader(csvfile)
-        for row in readCSV:
-            if row[0] != 'Название':
-                cur.execute("""insert or ignore into water(Name, Ca, Mg, F) 
-                values (?, ?, ?, ?)""", (' '.join(row[0], row[1], row[2], row[3])))
-                conn.commit()
-            
-    return jsonify({'Action': 'Refresh data', 'State': 'Success'})
+	cur.execute('delete from water')
+	conn.commit()
+	cur.execute("delete from sqlite_sequence where name='water'")
+	conn.commit()
+	with open('water.csv') as csvfile:
+		readCSV = csv.reader(csvfile)
+		for row in readCSV:
+			if row[0] != 'Название':
+				cur.execute("""insert or ignore into water(Name, Ca, Mg, F) 
+				values (?, ?, ?, ?)""", (' '.join(row[0], row[1], row[2], row[3])))
+				conn.commit()
+			
+	return jsonify({'Action': 'Refresh data', 'State': 'Success'})
 
 
 @app.route('/products')
 @auth.login_required
 @db_connect
 def get_all_products(cur, conn):
-    format_data = request.args.get('format', default='Ca,Mg,F') # bilki,zhiri,uglevodi,voda,kkal
-    if len(format_data) > 0:
-        format_data = ','+format_data
-    find = request.args.get('find', default=None)
-    if set(format_data.split(',')) > set('water_id,calcium,magnium,ftor'.split(',')):  # Все эл-ты format_data принадлежат всем возможным элементам
-        return jsonify({'Action': 'get all products', 'State': 'Error'})
-    cur.execute("select Name {} from water".format(format_data))
-    water = {i[0]: dict(zip(format_data.split(','), i[1:])) for i in cur.fetchall() if find is None or i[0] == find}
-    return jsonify(water)
+	format_data = request.args.get('format', default='Ca,Mg,F') # bilki,zhiri,uglevodi,voda,kkal
+	if len(format_data) > 0:
+		format_data = ','+format_data
+	find = request.args.get('find', default=None)
+	if set(format_data.split(',')) > set('water_id,calcium,magnium,ftor'.split(',')):  # Все эл-ты format_data принадлежат всем возможным элементам
+		return jsonify({'Action': 'get all products', 'State': 'Error'})
+	cur.execute("select Name {} from water".format(format_data))
+	water = {i[0]: dict(zip(format_data.split(','), i[1:])) for i in cur.fetchall() if find is None or i[0] == find}
+	return jsonify(water)
 
 @app.route('/users', methods=['POST'])
 @db_connect
 def signup(cur, conn):
-    username = request.args.get('username', default=None)
-    password = request.args.get('password', default=None)
-    if None in (username, password):
-        return jsonify({'Action': 'Register user', 'State': 'Error'})
-    else:
-        cur.execute('insert into users(username, password) values(?, ?)', (username, generate_password_hash(password)))
-        conn.commit()
-        return jsonify({'Action': 'Register user', 'State': 'Success'})
+	username = request.form.get('username', default=None)
+	password = request.form.get('password', default=None)
+	if None in (username, password):
+		return jsonify({'Action': 'Register user', 'State': 'Error'})
+	else:
+		cur.execute('insert into users(username, password) values(?, ?)', (username, generate_password_hash(password)))
+		conn.commit()
+		return jsonify({'Action': 'Register user', 'State': 'Success'})
 
+@app.route('/login', methods=['POST'])
+@db_connect
+def login(cur, conn):
+	username = request.form.get('username', default=None)
+	password = request.form.get('password', default=None)
+	if None in (username, password):
+		return jsonify({'Action': 'Login user', 'State': 'Error'})
+	else:
+		if verify_password(username,password):
+			auth.login()
+			return jsonify({'Action': 'Login user', 'State': 'Success'})
+		else:
+			jsonify({'Action': 'Login user', 'State': 'Not Found'})
 
 @app.route('/users', methods=['GET'])
 @auth.login_required
 @db_connect
-def login(cur, conn):
-    users = cur.execute("select userid, username from users").fetchall()
-    users_json = [dict(zip(('userid', 'username'), user)) for user in users]
-    return jsonify(users_json)
+def users(cur, conn):
+	users = cur.execute("select userid, username from users").fetchall()
+	users_json = [dict(zip(('userid', 'username'), user)) for user in users]
+	return jsonify(users_json)
 
 
 @app.route('/users/<userid>', methods=['PUT', 'GET', 'DELETE'])
 @auth.login_required
 @db_connect
 def user(cur, conn, userid):
-    try:
-        userid = int(userid)
-    except ValueError:
-        return jsonify({'Action': 'User', 'State': 'Error'})
-    if request.method == 'PUT':
-        password = request.args.get('password', default=None)
-        if password is None or userid != session['userid']:
-            return jsonify({'Action': 'Edit user', 'State': 'Error'})
-        else:
-            cur.execute('update users set password = ? where userid = ?',
-                        (generate_password_hash(password), userid))
-            conn.commit()
-            return jsonify({'Action': 'Edit user', 'State': 'Success'})
-    elif request.method == 'GET':
-        users = cur.execute("select userid, username from users where userid = ?", (userid, )).fetchall()
-        users_json = [dict(zip(('userid', 'username'), user)) for user in users]
-        return jsonify(users_json)
-    elif request.method == 'DELETE':
-        userid = request.args.get('userid', default=None)
-        if userid == session['userid']:
-            cur.execute('delete from users where userid = ?', (userid ,))
-            conn.commit()
-            return jsonify({'Action': 'Delete user', 'State': 'Success'})
-        else:
-            return jsonify({'Action': 'Delete user', 'State': 'Error'})
+	try:
+		userid = int(userid)
+	except ValueError:
+		return jsonify({'Action': 'User', 'State': 'Error'})
+	if request.method == 'PUT':
+		password = request.args.get('password', default=None)
+		if password is None or userid != session['userid']:
+			return jsonify({'Action': 'Edit user', 'State': 'Error'})
+		else:
+			cur.execute('update users set password = ? where userid = ?',
+						(generate_password_hash(password), userid))
+			conn.commit()
+			return jsonify({'Action': 'Edit user', 'State': 'Success'})
+	elif request.method == 'GET':
+		users = cur.execute("select userid, username from users where userid = ?", (userid, )).fetchall()
+		users_json = [dict(zip(('userid', 'username'), user)) for user in users]
+		return jsonify(users_json)
+	elif request.method == 'DELETE':
+		userid = request.args.get('userid', default=None)
+		if userid == session['userid']:
+			cur.execute('delete from users where userid = ?', (userid ,))
+			conn.commit()
+			return jsonify({'Action': 'Delete user', 'State': 'Success'})
+		else:
+			return jsonify({'Action': 'Delete user', 'State': 'Error'})
 
 
 @app.route('/')
 def index():
-    return "<h1>IT works !!</h1>"
+	return "<h1>IT works !!</h1>"
 
 @db_connect
 def init(cur, conn):
-    cur.execute("""
-        create table IF NOT EXISTS users (
-         userid INTEGER PRIMARY KEY,
-         username TEXT NOT NULL UNIQUE,
-         password TEXT NOT NULL
-        )
-        """)
-    conn.commit()
-    cur.execute("""
-        create table IF NOT EXISTS water (
-         waterid INTEGER PRIMARY KEY,
-         Name TEXT NOT NULL UNIQUE,
-         Ca TEXT NOT NULL,
-         Mg TEXT NOT NULL,
-         F TEXT NOT NULL
-        )
-        """)
-    conn.commit()
+	cur.execute("""
+		create table IF NOT EXISTS users (
+		 userid INTEGER PRIMARY KEY,
+		 username TEXT NOT NULL UNIQUE,
+		 password TEXT NOT NULL
+		)
+		""")
+	conn.commit()
+	cur.execute("""
+		create table IF NOT EXISTS water (
+		 waterid INTEGER PRIMARY KEY,
+		 Name TEXT NOT NULL UNIQUE,
+		 Ca TEXT NOT NULL,
+		 Mg TEXT NOT NULL,
+		 F TEXT NOT NULL
+		)
+		""")
+	conn.commit()
 
 port = os.environ.get('PORT')
 
 if __name__ == '__main__':
-    init()
-    app.run(host= '0.0.0.0', threaded=True, port=port)
+	init()
+	app.run(host= '0.0.0.0', threaded=True, port=port)
 
 """
 
 refresh_db          - POST              - обновляет данные в бд о продуктах. Не принимает аргументов
 
 products            - GET               - Отображает для пользователя все продукты. 
-                                            Аргумент format задаёт последовательность/полноту данных для пользователя
-                                            Аргумент find задаёт фразу для поиска по словарю
+											Аргумент format задаёт последовательность/полноту данных для пользователя
+											Аргумент find задаёт фразу для поиска по словарю
 
 users               - POST/GET          - добавляет пользователя.
-                                            Аргумент usename 
-                                            Аргумент password
-                                        - Отображает всех пользователей
+											Аргумент usename 
+											Аргумент password
+										- Отображает всех пользователей
 
 users/<userid>      - GET/PUT/DELETE    - Отображает пользователя с таким ИД, может быть не вашим
-                                        - Редактирует пользователя с таким ИД, если он ваш
-                                            Аргумент password
-                                        - Удаляет пользователя с таким ИД, если он ваш
+										- Редактирует пользователя с таким ИД, если он ваш
+											Аргумент password
+										- Удаляет пользователя с таким ИД, если он ваш
 
 """
